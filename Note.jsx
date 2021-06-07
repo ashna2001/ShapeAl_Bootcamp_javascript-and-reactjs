cfb472d253c4e27380aee88e59d76032edb26a10
ashna2001 <85478268+ashna2001@users.noreply.github.com>
2021-06-07 12:28:34 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note(){
+   return(
+     <div className="note">
+         <h1>Javascript and react.js</h1>
+         <p>class was ausem.The way of taking so help.good teaching method.</p>
+     </div>
+    );
+  }
+
+
+export default Note;
